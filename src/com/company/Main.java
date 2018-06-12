f99460fc75830d95c292be0c581d89b35076904e
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-06-12 12:14:42 -0400

    added instructions and random number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
+/*
+give instructions
+pick a random number
+get a guess from the user
+compare guess to the number
+ notify user of higher or lower  or ... that they won, do they want to play again?
+ */
+
+
+
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
 
+         System.out.println("Welcome To the Guessing Game! I'm thinking of a number between 1 and 100. can you figure it out?");
 
+//         pick a random number
+        int randomNumber = (int) Math.ceil(Math.random() * 100);
+        System.out.println(randomNumber);
 
 
 
