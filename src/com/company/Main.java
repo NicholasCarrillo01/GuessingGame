a12038887b62b0cae7b06b00fb820006ed5881de
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-06-12 11:46:31 -0400

    guess a number 1 to 100

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    
+        // write your code here
 
 
 
 
 
-
-
-
-
+//        HOW TO GET A USABLE RANDOM NUMBER
+//        double randomNumber;
+//        randomNumber = Math.random();
+//        System.out.println("the Random Number Is: " + randomNumber);
+//
+//        double randomNumberTimesHundred = randomNumber * 100;
+//        System.out.println("the Random Number Times Hundred" + randomNumberTimesHundred);
+//
+//        int randomNumberRoundedToCeil = (int) Math.ceil(randomNumber);
+//        System.out.println("The Rounded Ceil is: " + randomNumberTimesHundred);
+//
+//        int randomNumberRoundedToFloor = (int) Math.floor(randomNumberTimesHundred);
+//        System.out.println("The Rounded To Floor is: " + randomNumberTimesHundred);
+//
+//        int randomNumberRoundedNormally = (int) Math.round(randomNumberTimesHundred);
+//        System.out.println("The Rounding that we're used to: " + randomNumberRoundedNormally);
 
 
 
